d9b9015590bf83e4facc384b6bd6008426b8f855
Juan Lee <ninejuan@protonmail.com>
2025-02-25 02:20:30 +0900

    build: vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 4173,
+    strictPort: true,
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
+  server: {
+    cors: {
+      origin: ["https://peq.us", "https://api.peq.us"],
+      methods: ["POST", "GET", "PATCH", "PUT", "OPTIONS", "DELETE"],
+      allowedHeaders: ["*"],
+    },
+    allowedHosts: ["https://peq.us", "https://api.peq.us"],
+  },
+});
